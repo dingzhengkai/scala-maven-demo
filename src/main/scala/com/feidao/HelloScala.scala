6d4071f3131b58fa2429a019256d02b656208e35
dingzhengkai <dingzhengkai@ifeidao.com>
2019-12-27 15:30:41 +0800

    scala-maven-demo

---
+++ src/main/scala/com/feidao/HelloScala.scala
@@ -0,0 +1,12 @@
+package com.feidao
+
+object HelloScala {
+  
+  def foo(x : Array[String]): String = x.foldLeft("")((a, b) => a + b)
+  
+  def main(args : Array[String]) {
+    println( "Hello Scala World!" )
+    println("concat arguments = " + foo(args))
+  }
+
+}
